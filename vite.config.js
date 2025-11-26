b6310b4ced31044ad4060b6825db2fa904a9cbdf
Flavvgarcia <flavio_garciaherrera@hotmail.com>
2025-11-25 20:33:41 -0600

    Deploy usando docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Asistente-Domotico/',  // 👈 nombre exacto de tu repo
+  base: '/Asistente-Domotico/',
+  build: {
+    outDir: 'docs'
+  }
 })
